b000a963bc4c10f6cc7f611bbae1317d6ff6976b
wiktor.kobiela <wuch.mail@gmail.com>
2020-05-30 10:20:49 +0200

    ref #menu-bar some visual improvements

--- src/main/java/com/kobiela/Form.java
+++ src/main/java/com/kobiela/Form.java
@@ -10,6 +10,19 @@ import java.awt.event.MouseEvent;
 public class Form {
 
     public static void main(String[] args) {
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
+
+
         JFrame frame = new JFrame("Distance calculator");
         frame.setContentPane(new Form().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,6 +55,12 @@ public class Form {
     private JMenu About;
 
     public Form() {
+
+
+
+
+
+
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
